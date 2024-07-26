061fa91559918ce8b6102f592f21115a66ed3578
yongkingg <yongkingg8064@gmail.com>
2024-07-26 06:01:02 +0000

    Feat : 일정 수직 로직 추가

--- JS/SelectSchedulePage.js
+++ JS/SelectSchedulePage.js
@@ -1,80 +1,91 @@
 var editButtons = document.getElementsByClassName("edit_schedule");
-Array.from(editButtons).forEach(function (editButton) {
-  editButton.addEventListener("click", function () {
-    var scheduleDiv = this.closest(".schedule");
-    var scheduleContent = scheduleDiv.querySelector(".schedule_content");
+Array.from(editButtons).forEach((element) => {
+  element.addEventListener("click", editEvent);
+});
+
+function editEvent(event) {
+  var scheduleDiv = event.target.closest(".schedule");
+  var scheduleContent = scheduleDiv.querySelector(".schedule_content");
+  if (scheduleContent) {
     var deleteBtn = scheduleDiv.querySelector(".delete_schedule");
     var itemBox = scheduleDiv.querySelector(".schedule_item_box");
-
-    if (!scheduleContent) {
-      return;
-    }
-
-    if (scheduleContent.tagName === "P") {
-      toggleContentTag(scheduleDiv, scheduleContent, true);
-      if (itemBox && deleteBtn) {
-        toggleButtonState(itemBox, deleteBtn, "취소");
-      }
-    } else if (scheduleContent.tagName === "INPUT") {
-      if (confirm("수정을 취소하시겠습니까?")) {
-        toggleContentTag(scheduleDiv, scheduleContent, false);
-        if (itemBox) {
-          toggleButtonState(itemBox, this, "삭제");
-        }
-      }
-    }
-  });
-});
-
-var deleteButtons = document.getElementsByClassName("delete_schedule");
-Array.from(deleteButtons).forEach((element) => {
-  element.addEventListener("click", () => {
-    var confirmAlert = confirm("일정을 삭제하시겠습니까?");
-    if (confirmAlert) {
-      location.href = "../PAGE/SelectSchedulePage.jsp?key=0";
-      alert("일정이 삭제되었습니다.");
-    }
-  });
-});
-
-function toggleContentTag(parentElement, content, toInput) {
-  if (toInput) {
-    // p to input
-    var inputTag = document.createElement("input");
-    inputTag.value = content.textContent;
-    inputTag.classList.add("edit_schedule_input", "bold_text");
-    inputTag.dataset.originalText = content.textContent; // 기존 값 저장
-    parentElement.replaceChild(inputTag, content);
-  } else {
-    // input to p
-    var originalText = content.dataset.originalText;
-    var tmpPTag = document.createElement("p");
-    tmpPTag.textContent = originalText;
-    tmpPTag.classList.add("schedule_content");
-    parentElement.replaceChild(tmpPTag, content);
+    switchContentTag(scheduleDiv, scheduleContent, true);
+    switchButton(itemBox, deleteBtn, "취소");
+  } else if (scheduleDiv.querySelector("input")) {
+    var content = scheduleDiv.querySelector("input");
+    switchContentTag(scheduleDiv, content, false, true);
   }
 }
 
-function toggleButtonState(parentElement, button, state) {
-  var newButton = document.createElement("button");
-  newButton.classList.add("delete_schedule", "bold_text");
+var deleteButtons = document.getElementsByClassName("delete_schedule");
+Array.from(deleteButtons).forEach((element) => {
+  element.addEventListener("click", deleteEvent);
+});
 
+function deleteEvent() {
+  if (confirm("일정을 삭제하시겠습니까?")) {
+    location.href = "../PAGE/SelectSchedulePage.jsp?key=0";
+    alert("일정이 삭제되었습니다.");
+  }
+}
+
+// 컨텐츠 태그 전환
+function switchContentTag(parentElement, content, toInput) {
+  if (toInput) {
+    var inputTag = createInputTag(content);
+    parentElement.replaceChild(inputTag, content);
+  } else {
+    var pTag = createParagraphTag(content);
+    parentElement.replaceChild(pTag, content);
+  }
+}
+
+// 입력 태그 생성
+function createInputTag(content) {
+  var inputTag = document.createElement("input");
+  inputTag.value = content.textContent;
+  inputTag.classList.add("edit_schedule_input", "bold_text");
+  inputTag.dataset.originalText = content.textContent; // 기존 값 저장
+  return inputTag;
+}
+
+// 문단 태그 생성
+function createParagraphTag(content) {
+  var originalText;
+  if (content.value == content.dataset.originalText) {
+    originalText = content.dataset.originalText;
+  } else {
+    originalText = content.value;
+  }
+  var pTag = document.createElement("p");
+  pTag.textContent = originalText;
+  pTag.classList.add("schedule_content");
+  return pTag;
+}
+
+// 버튼 생성
+function createButton(state) {
+  var button = document.createElement("button");
+  button.classList.add("delete_schedule", "bold_text");
+  button.innerText = state;
+  return button;
+}
+
+// 버튼 전환
+function switchButton(parentElement, button, state) {
+  var newButton = createButton(state);
   if (state === "취소") {
-    newButton.innerText = "취소";
-    newButton.addEventListener("click", function () {
+    newButton.addEventListener("click", () => {
       if (confirm("수정을 취소하시겠습니까?")) {
         var inputTag = parentElement.parentElement.querySelector(
           ".edit_schedule_input"
         );
         if (inputTag) {
-          toggleContentTag(parentElement.closest(".schedule"), inputTag, false);
+          switchContentTag(parentElement.closest(".schedule"), inputTag, false);
         }
-        toggleButtonState(parentElement, this, "삭제");
+        switchButton(parentElement, newButton, "삭제");
       }
     });
-  } else if (state === "삭제") {
-    newButton.innerText = "삭제";
   }
-
   parentElement.replaceChild(newButton, button);
 }
